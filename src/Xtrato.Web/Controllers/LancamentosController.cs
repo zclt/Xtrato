efe6ba1bc2ce6d671fd710ba35bd0baec531c6e2
Matheus Antonio Zucolotto Pereira <matheus.pereira@softplan.com.br>
2024-06-19 18:35:27 -0300

    feat(model): Adiciona MVC Lancamento para banco inMemory

---
+++ src/Xtrato.Web/Controllers/LancamentosController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Xtrato.Web.Context;
+using Xtrato.Web.Models;
+
+namespace Xtrato.Web.Controllers
+{
+    public class LancamentosController : Controller
+    {
+        private readonly XtratoDbContext _context;
+
+        public LancamentosController(XtratoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lancamentos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Lancamentos.ToListAsync());
+        }
+
+        // GET: Lancamentos/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lancamento = await _context.Lancamentos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lancamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(lancamento);
+        }
+
+        // GET: Lancamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lancamentos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descricao,Data,Valor")] Lancamento lancamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lancamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lancamento);
+        }
+
+        // GET: Lancamentos/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lancamento = await _context.Lancamentos.FindAsync(id);
+            if (lancamento == null)
+            {
+                return NotFound();
+            }
+            return View(lancamento);
+        }
+
+        // POST: Lancamentos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Descricao,Data,Valor")] Lancamento lancamento)
+        {
+            if (id != lancamento.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lancamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LancamentoExists(lancamento.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lancamento);
+        }
+
+        // GET: Lancamentos/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lancamento = await _context.Lancamentos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (lancamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(lancamento);
+        }
+
+        // POST: Lancamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var lancamento = await _context.Lancamentos.FindAsync(id);
+            if (lancamento != null)
+            {
+                _context.Lancamentos.Remove(lancamento);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LancamentoExists(string id)
+        {
+            return _context.Lancamentos.Any(e => e.Id == id);
+        }
+    }
+}
--- src/Xtrato.Web/Program.cs
+++ src/Xtrato.Web/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Xtrato.Web.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<XtratoDbContext>(options => 
+    options.UseInMemoryDatabase("xtratodb"));
 
 var app = builder.Build();
 
